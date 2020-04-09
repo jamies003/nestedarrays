fdb46f40e6a9d35ed84ca7b3501dca9867500f8d
Jamie Stewart <stwjme@gmail.com>
2020-04-09 19:24:33 -0400

    Added Arrays

---
+++ nestedarrays.js
@@ -0,0 +1,5 @@
+
+let candies = ["Fruity", "Chocolate", "Peanut Butter"];
+
+let candyTypes = [["Fruit roll ups", "Life Savers", "Skittles"], ["Hersheys", "Cadbery", "Milky Way"], 
+["Reeses", "Butterfinger", "Snickers"]];
